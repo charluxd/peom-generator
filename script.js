c0730957dc49225b5ef49d18766df99b52e15425
charlx99 <charlotteuxd@gmail.com>
2024-02-20 14:54:00 +0800

    Updated Javascript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// alert("Hello!");
+function generatePoem(event) {
+  event.preventDefault();
+
+  let poemElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings:
+      "Demain, dès l’aube, à l’heure où blanchit la campagne, Je partirai.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let peomForm = document.querySelector("#poem-form");
+peomForm.addEventListener("submit", generatePoem);
